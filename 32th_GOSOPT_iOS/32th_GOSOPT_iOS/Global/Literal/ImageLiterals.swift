73c0e83c5d3bed7251ac4db8f0b409ca41d18fd4
Choi YoungLyn <joeuni0129@gmail.com>
2023-04-05 02:33:11 +0900

    [Add] #1-이미지 에셋 추가

---
+++ 32th_GOSOPT_iOS/32th_GOSOPT_iOS/Global/Literal/ImageLiterals.swift
@@ -0,0 +1,15 @@
+//
+//  ImageLiterals.swift
+//  32th_GOSOPT_iOS
+//
+//  Created by 최영린 on 2023/04/05.
+//
+
+import UIKit
+
+struct ImageLiterals {
+    
+    struct first_assignment {
+        static let myImage = UIImage(named: "MyImage")
+    }
+}
