c8b796dc465f31e6f05824d08567fc8f123ad3df
Choi YoungLyn <joeuni0129@gmail.com>
2023-04-04 23:05:02 +0900

    [Setting] #1-프로젝트 폴더링

---
+++ 32th_GOSOPT_iOS/32th_GOSOPT_iOS/Global/Literal/StringLiterals.swift
@@ -0,0 +1,12 @@
+//
+//  StringLiterals.swift
+//  32th_GOSOPT_iOS
+//
+//  Created by 최영린 on 2023/04/04.
+//
+
+import Foundation
+
+struct I18N {
+    
+}
---
+++ 32th_GOSOPT_iOS/32th_GOSOPT_iOS/Presentation/1st_Assignment/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  32th_GOSOPT_iOS
+//
+//  Created by 최영린 on 2023/04/04.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
