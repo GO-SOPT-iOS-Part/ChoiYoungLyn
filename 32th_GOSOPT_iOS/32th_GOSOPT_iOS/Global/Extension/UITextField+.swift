507ae81528f496c2663d74df96967777a4f53218
Choi YoungLyn <joeuni0129@gmail.com>
2023-04-05 01:30:02 +0900

    [Add] #1-Extension 추가

---
+++ 32th_GOSOPT_iOS/32th_GOSOPT_iOS/Global/Extension/UITextField+.swift
@@ -0,0 +1,22 @@
+//
+//  UITextField+.swift
+//  32th_GOSOPT_iOS
+//
+//  Created by 최영린 on 2023/04/05.
+//
+
+import UIKit
+
+extension UITextField {
+    func setLeftPadding(amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    
+    func setRightPadding(amount: CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
