be6a476b74046cdbcf3afbb3e3f8564a01599a65
Choi YoungLyn <joeuni0129@gmail.com>
2023-05-02 02:51:54 +0900

    [Add] #5 - CollectionView 관련 파일 추가

---
+++ TVING_CloneCoding/TVING_CloneCoding/Global/Protocols/UICollectionViewRegisterable.swift
@@ -0,0 +1,39 @@
+//
+//  UICollectionViewRegisterable.swift
+//  TVING_CloneCoding
+//
+//  Created by 최영린 on 2023/05/01.
+//
+
+import UIKit
+
+protocol UICollectionViewRegisterable {
+    static var isFromNib: Bool { get }
+    static func register(target: UICollectionView)
+}
+
+extension UICollectionViewRegisterable where Self: UICollectionViewCell {
+    static func register(target: UICollectionView) {
+        if self.isFromNib {
+          target.register(UINib(nibName: Self.className, bundle: nil), forCellWithReuseIdentifier: Self.className)
+        } else {
+          target.register(Self.self, forCellWithReuseIdentifier: Self.className)
+        }
+    }
+}
+
+protocol UICollectionHeaderViewRegisterable {
+    static var isFromNib: Bool { get }
+    static func register(target: UICollectionView)
+}
+
+extension UICollectionHeaderViewRegisterable where Self: UICollectionReusableView {
+    static func register(target: UICollectionView) {
+        if self.isFromNib {
+            target.register(UINib(nibName: Self.className, bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: Self.className)
+        } else {
+            target.register(Self.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: Self.className)
+        }
+    }
+}
+
