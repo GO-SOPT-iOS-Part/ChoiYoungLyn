2a197c7b639065ff2a8a12596bf4650171bc5d0a
Choi YoungLyn <joeuni0129@gmail.com>
2023-05-02 00:03:44 +0900

    [Feat] #5 - MyPage UI 구현

---
+++ TVING_CloneCoding/TVING_CloneCoding/Presentation/MyPageScene/VC/MyPageViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MyPageViewController.swift
+//  TVING_CloneCoding
+//
+//  Created by 최영린 on 2023/04/25.
+//
+
+import UIKit
+
+final class MyPageViewController: BaseViewController {
+    
+    // MARK: - UI Components
+     
+    private let myPageTableView = MyPageTableView()
+    
+    // MARK: - Life Cycles
+    
+    override func loadView() {
+        self.view = myPageTableView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setNavigationUI()
+    }
+}
+
+// MARK: - Extensions
+
+private extension MyPageViewController {
+    
+    func setNavigationUI() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: ImageLiterals.Common.beforeBtn, style: .plain, target: self, action: nil)
+        let notification = navigationItem.setRightButtonUI(self, imageName: UIImage(systemName: "bell"))
+        let setting = navigationItem.setRightButtonUI(self, imageName: UIImage(systemName: "gearshape"))
+        let spacer = UIBarButtonItem(barButtonSystemItem: .fixedSpace, target: nil, action: nil)
+        spacer.width = 9
+        navigationItem.rightBarButtonItems = [setting, spacer, notification]
+    }
+}
