fa4201e3e68f3dca10f088dcf0df7078f1f5062f
Choi YoungLyn <joeuni0129@gmail.com>
2023-05-10 22:39:58 +0900

    [Add] #7 - Entitiy 생성

---
+++ TVING_CloneCoding/TVING_CloneCoding/Network/DataModel/Home/TopRatedEntity.swift
@@ -0,0 +1,49 @@
+//
+//  TopRatedEntity.swift
+//  TVING_CloneCoding
+//
+//  Created by 최영린 on 2023/05/08.
+//
+
+import Foundation
+
+struct TopRatedEntity: Codable {
+    let page: Int
+    let results: [TopRatedResult]
+    let totalPages, totalResults: Int
+
+    enum CodingKeys: String, CodingKey {
+        case page, results
+        case totalPages = "total_pages"
+        case totalResults = "total_results"
+    }
+}
+
+// MARK: - Result
+struct TopRatedResult: Codable {
+    let adult: Bool
+    let backdropPath: String
+    let genreIDS: [Int]
+    let id: Int
+    let originalLanguage, originalTitle, overview: String
+    let popularity: Double
+    let posterPath, releaseDate, title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
